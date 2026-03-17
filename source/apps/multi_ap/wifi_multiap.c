e7bb8f8b176e430de09cb81dfec305d32ba367a0
amedpa922 <aishwaryaashok_medpalliwar@comcast.com>
2026-03-17 12:04:35 +0530

    setting is_device_type_xle() to true for local XB-XB testing

--- source/apps/multi_ap/wifi_multiap.c
+++ source/apps/multi_ap/wifi_multiap.c
@@ -49,7 +49,7 @@
 #define MAX_BUFF_SZ 1024
 #define MAX_IFACES 8
 #define ETH_P_1905 0x893a
-
+#define is_device_type_xle() true
 /* Timeout Macros */
 #define MULTIAP_RESP_TIMEOUT (1000)
 #define MULTIAP_CONNECT_TIMEOUT (60000 * 2)
